cfa875bb0c9f9bec5a2c718a3626df0d537a7146
Yuya Sumie <monamonacan0926@gmail.com>
2021-01-26 00:06:47 +0900

    シーン遷移をフェードアウト・インにした

---
+++ Assets/Scripts/CompanySceneController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CompanySceneController : MonoBehaviour
+{
+    private float step_time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        step_time = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 経過時間をカウント
+        step_time += Time.deltaTime;
+ 
+        // 1秒後に画面遷移( Title へ移動)
+        if (step_time >= 1.0f)
+        {
+            FadeManager.Instance.LoadScene("Scenes/Title", 0.3f);
+        }
+        
+    }
+}
