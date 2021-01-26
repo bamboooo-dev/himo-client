f6fdd84b5dc09ae39104fbeff6956acece2a881b
Yuya Sumie <monamonacan0926@gmail.com>
2021-01-26 22:24:00 +0900

    gRPC で Golang サーバーに接続できることを確認

---
+++ Assets/Scripts/GetRoomScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetRoomScript : MonoBehaviour
+{
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  // Ran when button is clicked
+  public void RunGetRoom(Text text)
+  {
+    ulong id = 1;
+    var response = GetRoomTest.GetRoom(id);
+    text.text = "Table: " + response.Table.Name;
+  }
+}
