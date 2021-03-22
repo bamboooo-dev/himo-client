3b56d05ed433eba3927bbbb91f2c7fee6021b38b
MONAKA0721 <monamonacan0926@gmail.com>
2021-03-22 21:03:20 +0900

    resolve Google AdMob

---
+++ Assets/Scripts/GoogleMobileAdsDemoScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+public class GoogleMobileAdsDemoScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Start()
+    {
+        MobileAds.Initialize(initStatus => { });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
