e11ff47d6ad9d776f5fcc77b6ded019b805c404a
Yuya Sumie <monamonacan0926@gmail.com>
2021-03-27 17:14:17 +0900

    FinalResult まで

--- Assets/Scripts/FinalResult/FinalResultManager.cs
+++ Assets/Scripts/FinalResult/FinalResultManager.cs
@@ -8,13 +8,19 @@ public class FinalResultManager : MonoBehaviour
   [SerializeField] private Text firstPlaceText;
   [SerializeField] private Text secondPlaceText;
   [SerializeField] private Text thirdPlaceText;
+  [SerializeField] private Button finishButton;
   private string[] sortedNames;
 
   void Start()
   {
+    // DEBUG
+    // RoomStatus.points = new int[] { 3, 2, 1, 4, 5 };
+    // Cycle.names = new string[] { "a", "b", "c", "d", "e" };
+
+    SortNames();
     Invoke(nameof(ShowThirdPlace), 1.0f);
-    Invoke(nameof(ShowSecondPlace), 1.0f);
-    Invoke(nameof(ShowFirstPlace), 1.0f);
+    Invoke(nameof(ShowSecondPlace), 2.0f);
+    Invoke(nameof(ShowFirstPlace), 3.0f);
   }
 
   private void ShowThirdPlace()
@@ -24,11 +30,12 @@ public class FinalResultManager : MonoBehaviour
 
   private void ShowSecondPlace()
   {
-    thirdPlaceText.text = this.sortedNames[1];
+    secondPlaceText.text = this.sortedNames[1];
   }
   private void ShowFirstPlace()
   {
-    thirdPlaceText.text = this.sortedNames[0];
+    firstPlaceText.text = this.sortedNames[0];
+    finishButton.gameObject.SetActive(true);
   }
 
   private void SortNames()
@@ -37,7 +44,7 @@ public class FinalResultManager : MonoBehaviour
     int[] sortedPoints = RoomStatus.points.OrderByDescending(x => x).ToArray();
     for (int i = 0; i < RoomStatus.points.Length; i++)
     {
-      names[i] = Cycle.names[Array.IndexOf(sortedPoints, RoomStatus.points[i])];
+      names[Array.IndexOf(sortedPoints, RoomStatus.points[i])] = Cycle.names[i];
     }
     this.sortedNames = names;
   }
---
+++ Assets/Scripts/FinalResult/FinishButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishButton : MonoBehaviour
+{
+  public void OnClick()
+  {
+    RoomStatus.finished = true;
+    SceneManager.LoadScene("Home");
+  }
+}
