b1de42ccb26443ddde7f3bc798a5375bd3ab71ea
Yuya Sumie <monamonacan0926@gmail.com>
2021-04-14 23:04:12 +0900

    インタースティシャル広告試み

--- Assets/Scripts/03_Home/HomeManager.cs
+++ Assets/Scripts/03_Home/HomeManager.cs
@@ -11,32 +11,32 @@ public class HomeManager : MonoBehaviour
     {
       MobileAds.Initialize(initStatus => { });
       RequestInterstitial();
-      if (this.interstitial.IsLoaded())
-      {
-        this.interstitial.Show();
-      }
       RoomStatus.finished = false;
     }
   }
 
-
   private void RequestInterstitial()
   {
-    #if UNITY_ANDROID
-        string adUnitId = "ca-app-pub-3882323268333157/3017982134";
-    #elif UNITY_IPHONE
-        string adUnitId = "ca-app-pub-3882323268333157/5774151767";
-    #else
-        string adUnitId = "unexpected_platform";
-    #endif
+#if UNITY_ANDROID
+    string adUnitId = "ca-app-pub-3882323268333157/3017982134";
+#elif UNITY_IPHONE
+      string adUnitId = "ca-app-pub-3882323268333157/5774151767";
+#else
+      string adUnitId = "unexpected_platform";
+#endif
 
     // Initialize an InterstitialAd.
     this.interstitial = new InterstitialAd(adUnitId);
-
+    this.interstitial.OnAdLoaded += HandleInterstitialBasedVideoLoaded;
     // Create an empty ad request.
     AdRequest request = new AdRequest.Builder().Build();
     // Load the interstitial with the request.
     this.interstitial.LoadAd(request);
+  }
+
+  public void HandleInterstitialBasedVideoLoaded(object sender, EventArgs args)
+  {
+    interstitial.Show();
   }
 }
 
