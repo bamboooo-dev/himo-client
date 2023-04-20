c3501e61d23c66e08d28e922bf90c1deb05d0413
Yuya Sumie <monamonacan0926@gmail.com>
2023-04-20 22:16:48 +0900

    もういろいろアプデ

---
+++ Assets/Editor/AndroidBuildProcessor.cs
@@ -0,0 +1,18 @@
+// #if UNITY_ANDROID
+// using System.IO;
+// using UnityEditor.Build;
+// using UnityEditor.Build.Reporting;
+
+// namespace Editor
+// {
+//     public class AndroidBuildProcessor : IPreprocessBuildWithReport
+//     {
+//         public int callbackOrder => 0;
+
+//         // public void OnPreprocessBuild(BuildReport report)
+//         // {
+//         //     File.WriteAllText($"{report.summary.outputPath}/build.gradle.NEW", "");
+//         // }
+//     }
+// }
+// #endif
